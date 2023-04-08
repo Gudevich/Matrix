978311fd6e1e55b985b0d1e376dfa8cb75f0f781
Gudevich <Gudevich.as@gmail.com>
2023-04-09 00:48:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+import { Column } from "./column.js";
+
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+const FONT_SIZE = 16;
+let columns = [];
+let columnsCount = 0;
+
+initCanvasSize();
+initColumns();
+
+animate();
+
+function initCanvasSize() {
+  canvas.width = document.documentElement.clientWidth;
+  canvas.height = document.documentElement.clientHeight;
+}
+
+function initColumns() {
+  columnsCount = canvas.width / FONT_SIZE;
+  columns = [];
+  for (let i = 0; i < columnsCount; i++) {
+    columns.push(new Column(i * FONT_SIZE, FONT_SIZE, canvas.height, context));
+  }
+}
+
+function animate() {
+  context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  
+  context.fillStyle = 'green';
+  context.font = `bold ${FONT_SIZE}px monospace`;
+  columns.forEach(column => column.drawSymbol());
+
+  setTimeout(() => requestAnimationFrame(animate), 50);
+}
+
+window.addEventListener('resize', () => {initCanvasSize();initColumns();
+  context.clearRect(0, 0, canvas.width, canvas.height);
+});
